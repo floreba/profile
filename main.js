e49ed4c8091ebb2377c6375a19c8a4b31a6377f5
Florian Bartel <bartel.florian@live.de>
2022-06-22 14:22:25 +0200

    ...

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-function hide() {
-  x = document.getElementById('carou');
-  btnShow = document.getElementById('button-show');
-  btnHide = document.getElementById('button-hide');
+x = document.getElementById('carou');
+btnShow = document.getElementById('button-show');
+btnHide = document.getElementById('button-hide');
 
+function hide() {
   x.className = 'hide';
   btnShow.className = 'show button';
   btnHide.className = 'hide button';
@@ -10,23 +10,22 @@ function hide() {
 }
 
 function show() {
-  x = document.getElementById('carou');
-  btnHide = document.getElementById('button-hide');
-  btnShow = document.getElementById('button-show');
-
   x.className = 'show';
   btnHide.className = 'show button';
   btnShow.className = 'hide button';
 }
 
+console.log(location);
 
-let scroller = document.getElementById("scroller")
-scroller.addEventListener("scroll", function () {
-            // const scrollerWrapper = document.getElementById("scroller");
-            // // scrollPercent =
-            // //   (scrollerWrapper.scrollLeft /
-            // //     (scrollerWrapper.scrollWidth - scrollerWrapper.clientWidth)) *
-            // //   100;
-            // // document.getElementById("scroll-progress").style.width =
-            // //   scrollPercent + "%";
-          });
+// let scroller = document.getElementById("scroller")
+// scroller.addEventListener("scroll", function () {
+//             // const scrollerWrapper = document.getElementById("scroller");
+//             // // scrollPercent =
+//             // //   (scrollerWrapper.scrollLeft /
+//             // //     (scrollerWrapper.scrollWidth - scrollerWrapper.clientWidth)) *
+//             // //   100;
+//             // // document.getElementById("scroll-progress").style.width =
+//             // //   scrollPercent + "%";
+//           });
+
+// lwindow.location;
